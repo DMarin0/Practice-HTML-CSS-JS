839dbf3b3998963e5bb25f296d56512e48e7507a
DenMarino <deparamonov@gmail.com>
2020-02-11 19:15:22 +0300

    Функция сложение двух чисел через массив

--- JS/assets/js/app.js
+++ JS/assets/js/app.js
@@ -1,4 +1,26 @@
-alert( 'Interface'[0].toLowerCase() ); // 'i'
+function sumInput() {
+
+  let numbers = [];
+
+  while (true) {
+
+    let value = prompt("Введите число", 0);
+
+    // Прекращаем ввод?
+    if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum = sum + number;
+  }
+  return sum;
+}
+
+alert( sumInput() );
+
 
 /* alert( Math.random() );
 let a = Math.random();
